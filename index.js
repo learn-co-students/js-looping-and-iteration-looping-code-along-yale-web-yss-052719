2729c8f6e7c7824f9d1011b089a9a2bf3e380ca9
maxsunnyday <max.sun@yale.edu>
2019-07-08 14:28:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let cards = [];
+    for (let index = 0; index < array.length; index++) {
+        cards[index] = `Thank you, ${array[index]}, for the wonderful ${event} gift!`;
+    }
+    return cards
+}
+
+function countdown(number) {
+    while (number > -1) {
+        console.log(number)
+        number -= 1
+    }
+}
