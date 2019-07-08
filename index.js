7682a93d66afa74db732d6d3a8d97ed002b01d6d
MatthewP01 <matthew.peoples@yale.edu>
2019-07-08 13:34:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function countdown(){
+  let countdown = 10;
+  while (countdown >= 0) {
+    console.log(countdown)
+    countdown -= 1
+  }
+}
+
+function writeCards(){
+  const names = ['Lisa', 'Kaitlin', 'Jan']
+  const card_array = []
+  for (let i = 0; i < names.length; i++) {
+    card_array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return card_array
+}
